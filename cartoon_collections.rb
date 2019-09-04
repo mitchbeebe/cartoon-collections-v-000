e694ce3db1b491e0b79b937b0d4bc0358ec5825a
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 01:53:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,10 @@ def long_planeteer_calls(array) # code an argument here
   array.any? { |i| i.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |item|
+    return item if cheese_types.include?(item)
+  end
 end
