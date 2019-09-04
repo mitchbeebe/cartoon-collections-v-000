4203f268b63cee37fbe5104faec1de99a85d24c4
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-04 01:41:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array) # code an argument here
   # Your code here
   array.each_with_index do |dwarf, idx|
     puts "#{idx + 1} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
